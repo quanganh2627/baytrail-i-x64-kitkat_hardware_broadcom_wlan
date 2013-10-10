64e72038eff5d70f34dff5d85c8922a58f6f41f9
Quentin Casasnovas <quentinx.casasnovas@intel.com>
2013-10-10 12:23:34 +0200

    wpa_supplicant_lib: bcm: Multi-lib support for supplicant
    BZ: 150983
    
    Change-Id: I9dd5f184d64ef15d06581e0be6039c6b55497171
    Category: device enablement
    Domain: CWS.WIFI-Bluewoods
    Origin: 3rd-party bluewoods
    Upstream-Candidate: no, proprietary
    Signed-off-by: Quentin Casasnovas <quentinx.casasnovas@intel.com>

--- bcmdhd/wpa_supplicant_8_lib/driver_cmd_nl80211.c
+++ bcmdhd/wpa_supplicant_8_lib/driver_cmd_nl80211.c
@@ -36,7 +36,7 @@ static void wpa_driver_send_hang_msg(struct wpa_driver_nl80211_data *drv)
 	}
 }
 
-int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
+int bcm_wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
 				  size_t buf_len )
 {
 	struct i802_bss *bss = priv;
@@ -96,33 +96,33 @@ int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
 	return ret;
 }
 
-int wpa_driver_set_p2p_noa(void *priv, u8 count, int start, int duration)
+int bcm_wpa_driver_set_p2p_noa(void *priv, u8 count, int start, int duration)
 {
 	char buf[MAX_DRV_CMD_SIZE];
 
 	memset(buf, 0, sizeof(buf));
 	wpa_printf(MSG_DEBUG, "%s: Entry", __func__);
 	snprintf(buf, sizeof(buf), "P2P_SET_NOA %d %d %d", count, start, duration);
-	return wpa_driver_nl80211_driver_cmd(priv, buf, buf, strlen(buf)+1);
+	return bcm_wpa_driver_nl80211_driver_cmd(priv, buf, buf, strlen(buf)+1);
 }
 
-int wpa_driver_get_p2p_noa(void *priv, u8 *buf, size_t len)
+int bcm_wpa_driver_get_p2p_noa(void *priv, u8 *buf, size_t len)
 {
 	/* Return 0 till we handle p2p_presence request completely in the driver */
 	return 0;
 }
 
-int wpa_driver_set_p2p_ps(void *priv, int legacy_ps, int opp_ps, int ctwindow)
+int bcm_wpa_driver_set_p2p_ps(void *priv, int legacy_ps, int opp_ps, int ctwindow)
 {
 	char buf[MAX_DRV_CMD_SIZE];
 
 	memset(buf, 0, sizeof(buf));
 	wpa_printf(MSG_DEBUG, "%s: Entry", __func__);
 	snprintf(buf, sizeof(buf), "P2P_SET_PS %d %d %d", legacy_ps, opp_ps, ctwindow);
-	return wpa_driver_nl80211_driver_cmd(priv, buf, buf, strlen(buf) + 1);
+	return bcm_wpa_driver_nl80211_driver_cmd(priv, buf, buf, strlen(buf) + 1);
 }
 
-int wpa_driver_set_ap_wps_p2p_ie(void *priv, const struct wpabuf *beacon,
+int bcm_wpa_driver_set_ap_wps_p2p_ie(void *priv, const struct wpabuf *beacon,
 				 const struct wpabuf *proberesp,
 				 const struct wpabuf *assocresp)
 {
@@ -152,7 +152,7 @@ int wpa_driver_set_ap_wps_p2p_ie(void *priv, const struct wpabuf *beacon,
 			wpabuf_dup(cmd_arr[i].src) : NULL;
 		if (ap_wps_p2p_ie) {
 			os_memcpy(pbuf, wpabuf_head(ap_wps_p2p_ie), wpabuf_len(ap_wps_p2p_ie));
-			ret = wpa_driver_nl80211_driver_cmd(priv, buf, buf,
+			ret = bcm_wpa_driver_nl80211_driver_cmd(priv, buf, buf,
 				strlen(_cmd) + 3 + wpabuf_len(ap_wps_p2p_ie));
 			wpabuf_free(ap_wps_p2p_ie);
 			if (ret < 0)
