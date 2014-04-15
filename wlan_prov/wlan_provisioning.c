07aae6844e6f7ef92db58798330df05202d4274a
Matthieu MAUGER <matthieux.mauger@intel.com>
2014-04-15 14:27:34 +0200

    WLAN_PROV: enforce security to match Intel coding policies
    BZ: 186046
    
    In order to match Intel Secure Coding Policies in wlan_prov, this
    patch takes the following actions:
    
     - verbose log are only displayed in engineer build and hidden in
    user and user-debug variants.
    
     - nullness of functions parameters is now checked
    
     - snprintf replaces sprintf
    
    Change-Id: I91dbf9bc2dc31abc514b8717917e0827c52373d7
    Category: device enablement
    Domain: CWS.WIFI-Bluewoods
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Matthieu MAUGER <matthieux.mauger@intel.com>

--- wlan_prov/wlan_provisioning.c
+++ wlan_prov/wlan_provisioning.c
@@ -45,6 +45,12 @@ const unsigned char NullMacAddr[MAC_ADDRESS_LEN] = { 0, 0, 0, 0, 0, 0 };
 #define BUFF_SIZE 40
 const char CAL_file_name[] = "/config/wifi/mac.txt";
 
+#ifdef WLAN_PROV_DEBUG_LOG_ENABLED
+#define LOG_I(...) LOGI(__VA_ARGS__)
+#else
+#define LOG_I(...)
+#endif
+
 static int get_prov_mac_address(unsigned char** macAddr);
 static int cal_update_mac_file(unsigned char *MacAddr);
 static int cal_check_mac_file_exist();
@@ -69,7 +75,7 @@ int main(int argc, char **argv)
 	/* Read MAC address from Chaabi */
 	if (get_prov_mac_address(&ProvMacAddr) < 0) {
 		/* read error*/
-		LOGI("Provisioned MAC not found, request to randomize one");
+		LOG_I("Provisioned MAC not found, request to randomize one");
 		ProvMacAddr = (unsigned char *) malloc(MAC_ADDRESS_LEN);
 		if (ProvMacAddr == NULL) {
 			res = -1;
@@ -77,7 +83,7 @@ int main(int argc, char **argv)
 		}
 		memcpy(ProvMacAddr, NullMacAddr, MAC_ADDRESS_LEN);
 	} else
-		LOGI("Provisioned Mac address:  %02x:%02x:%02x:%02x:%02x:%02x",
+		LOG_I("Provisioned Mac address:  %02x:%02x:%02x:%02x:%02x:%02x",
 					ProvMacAddr[0], ProvMacAddr[1], ProvMacAddr[2],
 					ProvMacAddr[3], ProvMacAddr[4], ProvMacAddr[5]);
 
@@ -85,11 +91,11 @@ int main(int argc, char **argv)
 	if (ProvMacAddr && (memcmp(ProvMacAddr, NullMacAddr, MAC_ADDRESS_LEN) == 0)) {
 		/* Provisioned MAC address is null due to engineering mode or read error */
 		if (cal_check_mac_file_exist() > 0){
-			LOGI("mac address exist, Nothing to do..");
+			LOG_I("mac address exist, Nothing to do..");
 			goto end;
 		}
 
-		LOGI("Mac address doesn't exist, randomize one.");
+		LOG_I("Mac address doesn't exist, randomize one.");
 		cal_randomize_mac(ProvMacAddr);
 	}
 
@@ -97,7 +103,7 @@ int main(int argc, char **argv)
 		goto fatal;
 
 end:
-	LOGI("End ...");
+	LOG_I("End ...");
 	sync();
 
 	if(ProvMacAddr)
@@ -116,7 +122,7 @@ static int cal_check_mac_file_exist(){
 	int ret;
 	unsigned char macAddr[MAC_ADDRESS_LEN];
 
-	LOGI("%s Enter", __FUNCTION__);
+	LOG_I("%s Enter", __FUNCTION__);
 	fp = fopen(CAL_file_name, "r");
 
 	if (fp == NULL){
@@ -139,12 +145,13 @@ static int cal_create_new_mac_file(unsigned char *MacAddr){
 	char buff[BUFF_SIZE];
 	int ret, fd;
 
-	LOGI("%s Enter", __FUNCTION__);
+	LOG_I("%s Enter", __FUNCTION__);
 
 	if (MacAddr == NULL)
 		return -1;
 
-	ret = sprintf(buff, "%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx\n", MacAddr[0], MacAddr[1], MacAddr[2], MacAddr[3], MacAddr[4], MacAddr[5]);
+	ret = snprintf(buff, BUFF_SIZE, "%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx\n",
+			MacAddr[0], MacAddr[1], MacAddr[2], MacAddr[3], MacAddr[4], MacAddr[5]);
 	if (ret != (MAC_ADDRESS_LEN*2)+1 ){
 		LOGE("error on writing into the buffer");
 		return -1;
@@ -176,12 +183,15 @@ static int cal_create_new_mac_file(unsigned char *MacAddr){
 /* depending of technology used */
 static int get_prov_mac_address(unsigned char** macAddr) {
 	int ret = 0;
+	if (macAddr == NULL) {
+		return -1;
+	}
 #ifdef BUILD_WITH_CHAABI_SUPPORT
 	/* Read MAC address from Chaabi */
 	ret = get_customer_data(ACD_WLAN_MAC_ADDR_FIELD_INDEX,
 									(void ** const) macAddr);
 	if (ret != MAC_ADDRESS_LEN) {
-		LOGI("get_customer_data error: 0x%x", ret);
+		LOG_I("get_customer_data error: 0x%x", ret);
 		if (ret >0) {
 			free(*macAddr);
 			*macAddr = NULL;
@@ -193,7 +203,7 @@ static int get_prov_mac_address(unsigned char** macAddr) {
 	ret = tee_token_item_read(WLAN_TOKEN_DG_ID, WLAN_TOKEN_SG_ID, WLAN_TOKEN_ITEM_ID,
 								0, *macAddr, MAC_ADDRESS_LEN, 0);
 	if (ret != 0) {
-		LOGI("tee_token_item_read error: 0x%x", ret);
+		LOG_I("tee_token_item_read error: 0x%x", ret);
 		free(*macAddr);
 		*macAddr = NULL;
 		return -1;
@@ -212,11 +222,14 @@ static int cal_update_mac_file(unsigned char *MacAddr)
 	char buff[BUFF_SIZE];
 	int ret= 0, i;
 
-	LOGI("%s Enter", __FUNCTION__);
+	LOG_I("%s Enter", __FUNCTION__);
+	if (MacAddr == NULL) {
+		return -1;
+	}
 	fp = fopen(CAL_file_name, "r");
 
 	if (fp == NULL){
-		LOGI("mac file does not exist, create a new one");
+		LOG_I("mac file does not exist, create a new one");
 		return cal_create_new_mac_file(MacAddr);
 	}
 
@@ -230,16 +243,16 @@ static int cal_update_mac_file(unsigned char *MacAddr)
 
 	if (cal_parse_buff(buff, macAddr)) {
 		if (memcmp(MacAddr, macAddr, MAC_ADDRESS_LEN) != 0) {
-			LOGI("mac from file is is not aligned, mac has to be updated");
+			LOG_I("mac from file is is not aligned, mac has to be updated");
 			return cal_create_new_mac_file(MacAddr);
 		}
 		else {
-			LOGI("mac from file is aligned, nothing to do");
+			LOG_I("mac from file is aligned, nothing to do");
 			return 1;
 		}
 	}
 
-	LOGI("error on parsing mac, mac has to be updated");
+	LOG_I("error on parsing mac, mac has to be updated");
 
 	return cal_create_new_mac_file(MacAddr);
 }
@@ -249,8 +262,10 @@ static void cal_randomize_mac(unsigned char *MacAddr){
 
 	struct timeval tv;
 
-	LOGI("%s Enter", __FUNCTION__);
-
+	LOG_I("%s Enter", __FUNCTION__);
+	if (MacAddr == NULL) {
+		return;
+	}
 	gettimeofday(&tv, NULL);
 	srand(tv.tv_usec);
 
@@ -265,11 +280,14 @@ static void cal_randomize_mac(unsigned char *MacAddr){
 /* Parse and covert the buff array into the macAddr array*/
 static int cal_parse_buff(char *buff, unsigned char *macAddr){
 
+	if (buff == NULL || macAddr == NULL) {
+		return -1;
+	}
 	if( sscanf(buff, "%2hhx%2hhx%2hhx%2hhx%2hhx%2hhx\n", &macAddr[0], &macAddr[1], &macAddr[2], &macAddr[3], &macAddr[4], &macAddr[5]) == 6){
-			LOGI("parsed mac %02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
+			LOG_I("parsed mac %02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
 			return 1;
 	}
 
-	LOGI("Problems on parsing the mac address");
+	LOG_I("Problems on parsing the mac address");
 	return -1;
 }
