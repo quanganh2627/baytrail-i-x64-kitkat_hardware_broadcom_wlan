563c9ea6570e45a1c765d9034de474e3147315d2
Samuel Ortiz <sameo@linux.intel.com>
2014-10-24 03:25:29 +0200

    firmware: Add bcm4334x firmware and NVRAM file
    The NVRAM file is based on the FFRD8 one.
    
    Change-Id: I31d8f2bd2a2b2c43d715f1f907733faa56b8222c
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>
    Reviewed-on: https://android.intel.com/292673
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Poulain, Loic <loic.poulain@intel.com>
    Tested-by: Nunes, Philippe <philippe.nunes@intel.com>
    Reviewed-by: Ortiz, Samuel <samuel.ortiz@intel.com>
    Tested-by: Ortiz, Samuel <samuel.ortiz@intel.com>

--- bcmdhd/Android.mk
+++ bcmdhd/Android.mk
@@ -1,3 +1,5 @@
 ifeq ($(BOARD_WLAN_DEVICE),bcmdhd)
     include $(call all-subdir-makefiles)
 endif
+
+include hardware/broadcom/wlan/bcmdhd/firmware/bcm4334x/Android.mk
---
+++ bcmdhd/firmware/bcm4334x/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wifi_bcm4334x
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := \
+	fw_bcmdhd.bin \
+	bcmdhd.cal 
+
+include $(BUILD_PHONY_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fw_bcmdhd.bin
+LOCAL_SRC_FILES := fw_bcmdhd.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_MODULE_STEM := $(LOCAL_SRC_FILES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bcmdhd.cal
+LOCAL_SRC_FILES := bcmdhd.cal
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_MODULE_STEM := $(LOCAL_SRC_FILES)
+include $(BUILD_PREBUILT)
