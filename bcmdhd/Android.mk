5b842bb0c02f6ac1b2f26d746c994c96d800ed75
Ganesh Mahalingam <ganesh.mahalingam@intel.com>
2015-01-19 14:42:34 -0800

    REVERT: TEMP-WORKAROUND: Fix build errors for other targets
    Change-Id: I498034dead6ab3a75c29047e6f818e433eaca052
    Signed-off-by: Ganesh Mahalingam <ganesh.mahalingam@intel.com>
    Reviewed-on: https://android.intel.com:443/321082

--- bcmdhd/Android.mk
+++ bcmdhd/Android.mk
@@ -2,5 +2,7 @@ ifeq ($(BOARD_WLAN_DEVICE),bcmdhd)
     include $(call all-subdir-makefiles)
 endif
 
+ifneq ($(filter coho cohol,$(TARGET_BOARD_PLATFORM)),)
 include hardware/broadcom/wlan/bcmdhd/firmware/bcm4334x/Android.mk
 include hardware/broadcom/wlan/bcmdhd/wpa_supplicant_8_lib/Android.mk
+endif
