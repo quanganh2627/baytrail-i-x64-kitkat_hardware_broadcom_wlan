f14b33be62fa6d36b8a81d8f107261ecf70d075d
Loic Poulain <loic.poulain@intel.com>
2015-04-09 08:59:09 +0200

    broadcom: firmware: Use 4330 fw/cal set
    All the firmwares share the same name for now.
    Should be fixed soon. for now replace the current by the 4330 one.
    
    Change-Id: I2c82c9368293aab135330662545a7d9429049a02
    Signed-off-by: Loic Poulain <loic.poulain@intel.com>
    Reviewed-on: https://android.intel.com/352331
    Reviewed-by: Ortiz, Samuel <samuel.ortiz@intel.com>
    Reviewed-by: Wood, Brian J <brian.j.wood@intel.com>
    Tested-by: Wood, Brian J <brian.j.wood@intel.com>
    Reviewed-on: https://android.intel.com/353054
    Reviewed-by: Prin, Gaetan <gaetan.prin@intel.com>
    Tested-by: Prin, Gaetan <gaetan.prin@intel.com>

--- bcmdhd/firmware/bcm4334x/Android.mk
+++ bcmdhd/firmware/bcm4334x/Android.mk
@@ -5,8 +5,10 @@ LOCAL_MODULE := wifi_bcm4334x
 LOCAL_MODULE_OWNER := intel
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := \
-	fw_bcmdhd.bin \
-	bcmdhd.cal 
+	fw_bcmdhd_4330_b2 \
+	cal_bcmdhd_4330_b2 \
+	fw_bcmdhd_apsta_4330_b2 \
+	fw_bcmdhd_p2p_4330_b2
 
 include $(BUILD_PHONY_PACKAGE)
 
@@ -28,4 +30,44 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_MODULE_STEM := $(LOCAL_SRC_FILES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fw_bcmdhd_4330_b2
+LOCAL_SRC_FILES := fw_bcmdhd.bin_4330_b2
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_MODULE_STEM := fw_bcmdhd.bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cal_bcmdhd_4330_b2
+LOCAL_SRC_FILES := bcmdhd_aob.cal_4330_b2
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_MODULE_STEM := bcmdhd.cal
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fw_bcmdhd_apsta_4330_b2
+LOCAL_SRC_FILES := fw_bcmdhd_apsta.bin_4330_b2
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_MODULE_STEM := fw_bcmdhd_apsta.bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fw_bcmdhd_p2p_4330_b2
+LOCAL_SRC_FILES := fw_bcmdhd_p2p.bin_4330_b2
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_MODULE_STEM := fw_bcmdhd_p2p.bin
 include $(BUILD_PREBUILT)
